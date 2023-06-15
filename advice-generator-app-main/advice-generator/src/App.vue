99b0269f0d760eff14c3b0f9f4cca0cb496e6b8a
Marie Collinge <marielouisecollinge@gmail.com>
2023-06-15 17:24:12 +0100

    updated functionality to include get request to api

--- advice-generator-app-main/advice-generator/src/App.vue
+++ advice-generator-app-main/advice-generator/src/App.vue
@@ -1,18 +1,44 @@
 <script setup>
 import CardContainer from "./components/CardContainer.vue";
+import { ref, onMounted } from "vue";
+
+let response = ref();
+
+
+
+async function getAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
+
+async function refreshAdvice() {
+  response.value = await getAdvice();
+};
+
 function onClick(){
   console.log('clicked');
-}
+  refreshAdvice();
+};
+
+onMounted(async () => {
+  response.value = await getAdvice();
+});
 </script>
 
 <template>
-  <div id="app"><CardContainer @click="onClick"/></div>
+  <div v-if="response" id="app">
+  <CardContainer :title="response.slip.id" 
+  :body="response.slip.advice" 
+  @click="onClick"/>
+  </div>
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Manrope");
 body {
   font-family: "Manrope", sans-serif !important;
-  background-color: hsl(217, 19%, 24%) !important;
+  background-color: hsl(218, 23%, 16%) !important;
 }
 </style>
--- advice-generator-app-main/advice-generator/src/components/CardContainer.vue
+++ advice-generator-app-main/advice-generator/src/components/CardContainer.vue
@@ -1,14 +1,36 @@
+<script setup>
+defineProps({
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
+});
+</script>
+
+
 <template>
     <div class="card">
-        <h1>ADVICE #117</h1>
-        <p>"It is easy to sit up and take..."</p>
-        <button @click="$emit('click')">dice</button>
+        <h1>ADVICE #{{ title }}</h1>
+        <p>"{{ body }}"</p>
+        <img src="../assets/pattern-divider-desktop.svg"
+        id="desktop-divider"
+        />
+        <div class="button" >
+        <img 
+        src="../assets/icon-dice.svg"
+        @click="$emit('click')"
+        id="dice"
+        />
+    </div>
+        <!-- <button @click="$emit('click')">dice</button> -->
     </div>
 </template>
 
 <style scoped>
 .card {
-    background-color: hsl(217, 19%, 38%);
+    background-color: hsl(217, 19%, 24%);
     padding: 16px;
     border-radius: 6px;
     text-align: center;
@@ -18,16 +40,39 @@ h1 {
 color: hsl(150, 100%, 66%);
 font-size: 14px;
 font-weight: 800;
+letter-spacing: 5px;
 }
 
 p {
     color: hsl(193, 38%, 86%);
     font-size: 28px;
     font-weight: 800;
+    letter-spacing: 1px;
+    padding-top: 15px;
 }
 
-button {
+.button {
+background-color: hsl(150, 100%, 66%);
+border-radius: 50%;
+width: 60px;
+height: 60px;
+text-align: center;
+bottom: -40px;
+margin: auto;
+}
 
+.button:active {
+    box-shadow: 0px 0px 20px 5px hsl(150, 100%, 66%);
+    cursor: pointer;
+} 
+
+#desktop-divider {
+    padding-top: 30px;
+}
+
+#dice {
+    top: 50%;
+    transform: translate(0%, -50%);
 }
 </style>
 
