f16050cc9d7090981d177a080f050f4b8f836945
Marie Collinge <marielouisecollinge@gmail.com>
2023-06-15 11:38:03 +0100

    built vue app and card component

---
+++ advice-generator-app-main/advice-generator/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import CardContainer from "./components/CardContainer.vue";
+function onClick(){
+  console.log('clicked');
+}
+</script>
+
+<template>
+  <div id="app"><CardContainer @click="onClick"/></div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Manrope");
+body {
+  font-family: "Manrope", sans-serif !important;
+  background-color: hsl(217, 19%, 24%) !important;
+}
+</style>
---
+++ advice-generator-app-main/advice-generator/src/components/CardContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card">
+        <h1>ADVICE #117</h1>
+        <p>"It is easy to sit up and take..."</p>
+        <button @click="$emit('click')">dice</button>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: hsl(217, 19%, 38%);
+    padding: 16px;
+    border-radius: 6px;
+    text-align: center;
+}
+
+h1 {
+color: hsl(150, 100%, 66%);
+font-size: 14px;
+font-weight: 800;
+}
+
+p {
+    color: hsl(193, 38%, 86%);
+    font-size: 28px;
+    font-weight: 800;
+}
+
+button {
+
+}
+</style>
+
+
