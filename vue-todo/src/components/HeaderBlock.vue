24c02e4b6eb1f774f1ec21cab2c7dacf94dac163
Marie Collinge <marielouisecollinge@gmail.com>
2023-06-19 15:06:59 +0100

    feat: add the UI to the task manager

---
+++ vue-todo/src/components/HeaderBlock.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonTemplate @btn-click="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Add Task'" 
+        :color="showAddTask ? 'red' : 'green'"
+        />
+    </header>
+</template>
+
+<script>
+import ButtonTemplate from './ButtonTemplate.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        ButtonTemplate
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>
---
+++ vue-todo/src/components/TasksTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <TaskTemplate 
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)" 
+        :task="task"
+        />
+    </div>
+</template>
+
+<script>
+import TaskTemplate from './TaskTemplate.vue';
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: []
+    },
+    components: {
+        TaskTemplate
+    },
+    emits: ['delete-task', 'toggle-reminder']
+}
+</script>
